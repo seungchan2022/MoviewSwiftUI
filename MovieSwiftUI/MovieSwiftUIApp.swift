fda743b96680575287cbb068f38522e6e1418de6
승찬 <anstmdcks12@naver.com>
2023-07-06 15:10:53 +0900

    Initial Commit

---
+++ MovieSwiftUI/MovieSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSwiftUIApp.swift
+//  MovieSwiftUI
+//
+//  Created by 승찬 on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct MovieSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
